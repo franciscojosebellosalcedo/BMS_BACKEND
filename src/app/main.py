8e32ed0a7fc3d40c8101f3a3a3e6073c86d197ae
Francisco jose bello salcedo <franciscojosebellosalcedo@gmail.com>
2026-08-02 17:45:14 -0500

    workflow CI

--- src/app/main.py
+++ src/app/main.py
@@ -3,10 +3,7 @@ from app.routes.api_routes import api_routes
 from app.shared.exceptions.app_exception import AppException
 from app.shared.handlers.exception_handler import exception_handler
 from fastapi.middleware.cors import CORSMiddleware
-from create_tables import create_tables
 from app.core.config import settings
-
-create_tables()
 
 app = FastAPI(
     title="BMS API"
--- src/app/shared/models.py
+++ src/app/shared/models.py
@@ -1,7 +1,3 @@
-
-from app.core.database import Base
-from app.core.database import engine
-
 from app.modules.auth.infraestructure.persistence.user_model import UserModel
 from app.modules.setting.rol.infraestructure.persistence.rol_model import RolModel
 from app.modules.menu.infraestructure.persistence.module_model import ModuleModel
@@ -9,9 +5,3 @@ from app.modules.menu.infraestructure.persistence.submodule_model import Submodu
 from app.modules.menu.infraestructure.persistence.option_model import OptionModel
 from app.modules.auth.infraestructure.persistence.user_permission_model import UserPermissionModel
 from app.modules.setting.rol.infraestructure.persistence.rol_permission_model import RolPermissionModel
-
-def create_tables():
-
-    Base.metadata.create_all(
-        bind=engine
-    )
